25c83c43e569349da2c8f4a80ba8cb69e3430b3f
Jessa May Balita <jesssaaamay@gmail.com>
2025-09-02 11:09:36 +0800

    used neon for serverless environment config

--- lib/actions/cafemenu.actions.ts
+++ lib/actions/cafemenu.actions.ts
@@ -1,11 +1,10 @@
 "use server";
-import { PrismaClient } from "@/lib/generated/prisma";
+import { prisma } from "@/db/prisma";
 import { convertPrismaToJS } from "../utils";
 import { LATEST_MENU_ITEMS_LIMIT } from "../constants";
 
 // get latest cafemenu items
 export async function getLatestMenuItems() {
-  const prisma = new PrismaClient();
   const data = await prisma.cafeMenu.findMany({
     take: LATEST_MENU_ITEMS_LIMIT,
     orderBy: {
